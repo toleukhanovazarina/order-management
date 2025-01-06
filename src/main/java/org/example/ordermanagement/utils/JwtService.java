529fc92406720170454bf35bab1f3e6f0818206d
Zarina <zarina.toleukhanova@gmail.com>
2025-01-06 20:59:15 +0500

    feature/authentication: added JwtService for token management

---
+++ src/main/java/org/example/ordermanagement/utils/JwtService.java
@@ -0,0 +1,171 @@
+package org.example.ordermanagement.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+
+import org.example.ordermanagement.db.entity.Admin;
+import org.example.ordermanagement.db.entity.Customer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${token.signing.key}")
+    private String jwtSigningKey;
+
+    /**
+     * Извлечение юзернейм пользователя из токена
+     *
+     * @param token токен
+     * @return имя пользователя
+     */
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    /**
+     * Извлечение данных пользователя из токена
+     *
+     * @param token токен
+     * @return данные пользователя
+     */
+
+    public Map<String,String> extractTokenData(String token){
+        final Claims claims = extractAllClaims(token);
+        Map<String,String> tokenData = new HashMap<>();
+        tokenData.put("username",claims.get("username",String.class));
+        tokenData.put("role",claims.get("role",String.class));
+        tokenData.put("id", String.valueOf(claims.get("id",Long.class)));
+        return tokenData;
+    }
+
+    /**
+     * Генерация токена
+     *
+     * @param userDetails данные пользователя
+     * @return токен
+     */
+    public String generateToken(UserDetails userDetails){
+        Map<String,Object> claims = new HashMap<>();
+        if (userDetails instanceof Customer customUserDetails){
+            claims.put("id",customUserDetails.getId());
+            claims.put("role",customUserDetails.getRole());
+            claims.put("username",customUserDetails.getUsername());
+        }
+        return generateToken(claims,userDetails);
+    }
+
+    /**
+     * Генерация токена админа
+     *
+     * @param userDetails данные пользователя
+     * @return токен
+     */
+    public String generateTokenAdmin(UserDetails userDetails){
+        Map<String,Object> claims = new HashMap<>();
+        if (userDetails instanceof Admin customUserDetails){
+            claims.put("id",customUserDetails.getId());
+            claims.put("role",customUserDetails.getRole());
+        }
+        return generateToken(claims,userDetails);
+    }
+
+
+    /**
+     * Проверка токена на валидность
+     *
+     * @param token       токен
+     * @param userDetails данные пользователя
+     * @return true, если токен валиден
+     */
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    /**
+     * Извлечение данных из токена
+     *
+     * @param token           токен
+     * @param claimsResolvers функция извлечения данных
+     * @param <T>             тип данных
+     * @return данные
+     */
+    private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+   /**
+    * Генерация токена
+    *
+    * @param userDetails данные пользователя
+    * @return токен
+    */
+    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(Long.MAX_VALUE))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    /**
+     * Проверка токена на просроченность
+     *
+     * @param token токен
+     * @return true, если токен просрочен
+     */
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    /**
+     * Извлечение даты истечения токена
+     *
+     * @param token токен
+     * @return дата истечения
+     */
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    /**
+     * Извлечение всех данных из токена
+     *
+     * @param token токен
+     * @return данные
+     */
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * Получение ключа для подписи токена
+     *
+     * @return ключ
+     */
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
+
