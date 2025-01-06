e62da03185272e38ac3fe526db0b042951ae02a7
Zarina <zarina.toleukhanova@gmail.com>
2025-01-06 19:53:06 +0500

    Initial commit

---
+++ src/test/java/org/example/ordermanagement/OrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
