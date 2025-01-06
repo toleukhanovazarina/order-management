edc3b02c22751a5ad928997ce2041b3c76d2529c
Zarina <zarina.toleukhanova@gmail.com>
2025-01-06 22:27:02 +0500

    feature/order-crud: added Product entity

---
+++ src/main/java/org/example/ordermanagement/db/entity/Product.java
@@ -0,0 +1,26 @@
+package org.example.ordermanagement.db.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "quantity")
+    private int quantity;
+}
---
+++ src/main/java/org/example/ordermanagement/db/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.example.ordermanagement.db.repository;
+
+import org.example.ordermanagement.db.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
